c95d975fd72aeaa76603c320c1318d3d7a4d5840
David Heinemeier Hansson <david@hey.com>
2025-08-19 10:03:34 +0200

    Add online iso build option

---
+++ build_online_iso.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+set -e
+
+# Note that these are packages installed to the Arch container
+# used to build the ISO.
+pacman-key --init
+pacman --noconfirm -Sy archlinux-keyring
+pacman --noconfirm -Sy archiso git python-pip sudo base-devel jq wget
+
+cache_dir=$(realpath --canonicalize-missing ~/.cache/omarchy/iso_$(date +%Y-%m-%d))
+offline_mirror_dir="$cache_dir/airootfs/var/cache/omarchy/mirror/offline"
+
+# We need to fiddle with pip settings
+# in order to install to the correct place
+# as well as ignore some errors to make this less verbose
+export PIP_ROOT="$cache_dir/airootfs/"
+export PIP_ROOT_USER_ACTION="ignore"
+export PIP_NO_WARN_SCRIPT_LOCATION=1
+export PIP_BREAK_SYSTEM_PACKAGES=1
+
+# These packages will be installed **into** the ISO, and
+# won't be available to either archinstall or omarchy installer.
+python_packages=(
+  terminaltexteffects
+)
+arch_packages=(
+  git
+  gum
+  openssl
+  wget
+)
+
+mkdir -p $cache_dir/
+mkdir -p $offline_mirror_dir/
+
+# We base our ISO on the official arch ISO (releng) config
+cp -r archiso/configs/releng/* $cache_dir/
+
+# We clone the installer, and move it to the root users home folder
+# since this is the default user in the official releng ISO profile.
+git clone https://github.com/omacom-io/omarchy-installer "$cache_dir/airootfs/root/omarchy-installer"
+mv "$cache_dir/airootfs/root/omarchy-installer/installer" "$cache_dir/airootfs/root/installer"
+mv "$cache_dir/airootfs/root/omarchy-installer/logo.txt" "$cache_dir/airootfs/root/logo.txt"
+rm -rf "$cache_dir/airootfs/root/omarchy-installer"
+
+git clone -b dev --single-branch https://github.com/basecamp/omarchy.git "$cache_dir/airootfs/root/omarchy"
+
+# Copy in the connectivity check script
+cp /check_connectivity.sh "$cache_dir/airootfs/root/check_connectivity.sh"
+
+# We add in our auto-start applications
+# First we'll check for an active internet connection
+# Then we'll start the omarchy installer
+cat <<-_EOF_ | tee $cache_dir/airootfs/root/.automated_script.sh
+	#!/usr/bin/env bash
+
+	if [[ \$(tty) == "/dev/tty1" ]]; then
+	    sh ./check_connectivity.sh && \
+	    sh ./installer && \
+	    archinstall \
+	    	--config user_configuration.json \
+	    	--creds user_credentials.json \
+	    	--silent && \
+	    export OMARCHY_USER=\`ls /mnt/home/\` && \
+
+	    mkdir -p /mnt/home/\$OMARCHY_USER/.local/share/ && \
+	    cp -r /root/omarchy "/mnt/home/\$OMARCHY_USER/.local/share/" && \
+	    chown -R 1000:1000 "/mnt/home/\$OMARCHY_USER/.local/" && \
+	    chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install.sh && \
+	    chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/apps/mimetypes.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/apps/webapps.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/apps/xtras.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/detect-keyboard-layout.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/fix-fkeys.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/network.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/nvidia.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/power.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/timezones.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/config.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/identification.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/increase-sudo-tries.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/login.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/asdcontrol.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/bluetooth.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/fonts.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/hyprlandia.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/printer.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/theme.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/desktop.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/development.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/nvim.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/ruby.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/docker.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/firewall.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/terminal.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/migrations.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/aur.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/guard.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/gum.sh && \
+		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/tte.sh && \
+		echo '' > /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/tte.sh && \
+		echo '' > /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/gum.sh && \
+	    HOME=/home/\$OMARCHY_USER arch-chroot -u \$OMARCHY_USER /mnt/ /bin/bash -c "source /home/\$OMARCHY_USER/.local/share/omarchy/install.sh"
+	fi
+_EOF_
+
+# We patch permissions, grub and efi loaders to our liking:
+(cd $cache_dir/ && git apply /permissions.patch)
+(cd $cache_dir/ && git apply /grub-autoboot.patch)
+(cd $cache_dir/ && git apply /efi-autoboot.patch)
+# We could also use:
+# patch -p1 < aur-mirror.patch
+# patch -p1 < permissions.patch
+# patch -p1 < grub-autoboot.patch
+# patch -p1 < efi-autoboot.patch
+
+# Remove the default motd
+rm "$cache_dir/airootfs/etc/motd"
+
+# Install Python packages for the installer into the ISO
+# file system.
+pip install "${python_packages[@]}"
+
+# Add our needed packages to packages.x86_64
+printf '%s\n' "${arch_packages[@]}" >>"$cache_dir/packages.x86_64"
+
+# Because this weird glitch with archiso, we also need to sync down
+# all the packages we need to build the ISO, but we'll do that in the
+# "host" mirror location, as we don't want them inside the ISO taking up space.
+# We'll also remove tzupdate as it won't be found in upstream mirrors.
+iso_packages=($(cat "$cache_dir/packages.x86_64"))
+
+mkdir -p /tmp/cleandb
+
+# Finally, we assemble the entire ISO
+mkarchiso -v -w "$cache_dir/work/" -o "/out/" "$cache_dir/"
