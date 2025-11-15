dd35ef3c1f65605bd19800fe83421e72a2d9c57e
Karthik Kalidas <karthik.kalidas@gmail.com>
2025-11-15 09:00:34 -0600

    try another uncomment

--- builder/build-iso.sh
+++ builder/build-iso.sh
@@ -80,7 +80,7 @@ mkdir -p $offline_mirror_dir/
  # Download all the packages to the offline mirror inside the ISO
  mkdir -p /tmp/offlinedb
  pacman --config /configs/pacman-online.conf --noconfirm -Syw "${all_packages[@]}" --cachedir $offline_mirror_dir/ --dbpath /tmp/offlinedb
- repo-add --new "$offline_mirror_dir/offline.db.tar.gz" "$offline_mirror_dir/"*.pkg.tar.zst
+ # repo-add --new "$offline_mirror_dir/offline.db.tar.gz" "$offline_mirror_dir/"*.pkg.tar.zst
 
  # # Create a symlink to the offline mirror instead of duplicating it.
  # # mkarchiso needs packages at /var/cache/omarchy/mirror/offline in the container,
